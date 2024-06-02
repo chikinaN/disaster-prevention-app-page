4fa4cd64c082b24713c1b87ffe48010502f28725
chigayuki <107536212+chikinaN@users.noreply.github.com>
2024-06-02 13:40:09 +0900

    githubpage用

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
+
+
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
+
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/disaster-prevention-app-page/",
   plugins: [
     remix({
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html"),
+        );
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
