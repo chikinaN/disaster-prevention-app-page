8be136a5b7a6c32997fbe9c88f6b7f71d1350a1a
chigayuki <107536212+chikinaN@users.noreply.github.com>
2024-06-02 14:33:55 +0900

    tailwindのtest

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { MetaFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -5,20 +6,35 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import styles from "./tailwind.css?url";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "防災アプリ" },
+    { name: "description", content: "防災アプリのデモです" },
+  ];
+};
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+      <body className="m-0 p-0 bg-gray-200 font-normal">
+        <main className="relative w-1/2 h-screen mx-auto shadow-lg rounded-lg overflow-hidden">
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </main>
       </body>
     </html>
   );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-3xl font-bold underline text-rose-900">Welcome to Remix</h1>
       <ul>
         <li>
           <a
